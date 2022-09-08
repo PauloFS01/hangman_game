c9a98e231f47140866e69f344a95c777d394e2e8
Paulo Silva <paulosilva@Paulos-MacBook-Air.local>
2022-09-07 23:30:32 -0300

    elm basic program

--- Main.elm
+++ Main.elm
@@ -1,4 +1,39 @@
-import Html exposing (..)
+import Browser
+import Html exposing (Html, Attribute, div, input, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+type alias Model = 
+    { message : String }
+
+type Msg =
+    Message String
+
+--- UPDATE ---
+
+init : Model 
+init = 
+    { message = "This is initial state" }
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of 
+        Message newMessage ->
+            { model | message = newMessage }
+            
+--- VIEW ---
+
+view : Model -> Html Msg
+view model = 
+    div [] 
+    [ input [ placeholder "Type here!", onInput Message ] []
+    , div [] [text model.message]
+    ]
 
 main =
-    text "Hello World!"
+    Browser.sandbox
+    { init = init
+    , view = view
+    , update = update
+    }
